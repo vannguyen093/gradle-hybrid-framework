3ef2b9000dcdde88538359f0ca05e3890afca4dd
Van Nguyen <vannguyen093@gmail.com>
2022-11-27 11:59:32 +0700

    Turn Java FW into Gradle FW

--- src/test/java/data/UserDataMapper.java
+++ src/test/java/data/UserDataMapper.java
@@ -27,7 +27,7 @@ public class UserDataMapper {
         try {
             ObjectMapper mapper = new ObjectMapper();
             mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-            return mapper.readValue(new File(GlobalConstants.PROJECT_PATH + "/src/test/resrouce/UserData.json"), UserDataMapper.class);
+            return mapper.readValue(new File(GlobalConstants.PROJECT_PATH + "/src/test/resources/UserData.json"), UserDataMapper.class);
         } catch (Exception ex){
             ex.printStackTrace();
             return null;
